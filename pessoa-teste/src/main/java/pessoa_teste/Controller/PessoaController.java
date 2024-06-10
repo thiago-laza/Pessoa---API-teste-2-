abe77e7f5bd45372a9c13a58dbf46c15ddeef872
thiago-laza <thiago.profmat@gmail.com>
2024-06-09 22:27:47 -0300

    Home: ajuste controller.

--- pessoa-teste/src/main/java/pessoa_teste/Controller/PessoaController.java
+++ pessoa-teste/src/main/java/pessoa_teste/Controller/PessoaController.java
@@ -1,8 +1,6 @@
 package pessoa_teste.Controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
